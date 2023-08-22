a0f665924852119694fd1d5e538514a40e3c24c8
Megan Buck <thequiltingriverotter@gmail.com>
2023-08-21 23:06:35 -0400

    Refactored functions, made factory functions for tables, added basic DOM elements

--- app.js
+++ app.js
@@ -1,82 +1,113 @@
-
 //This object could be created dynamically with a function
-const multiplication = {
-    "1x1" : 1,
-    "1x2" : 2,
-    "1x3" : 3,
-    "1x4" : 4,
-    "1x5" : 5,
-    "1x6" : 6,
-    "1x7" : 7,
-    "1x8" : 8,
-    "1x9" : 9,
-    "1x10" : 10,
-    "1x11" : 11,
-    "1x12" : 12,
-    "2x1" : 2,
-    "2x2" : 4,
-    "2x3" : 6,
-    "2x4" : 8,
-    "2x5" : 10,
-    "2x6" : 12,
-    "2x7" : 14,
-    "2x8" : 16,
-    "2x9" : 18,
-    "2x10" : 20,
-    "2x11" : 22,
-    "2x12" : 24,
-    "3x1" : 3,
-    "3x2" : 6,
-    "3x3" : 9,
-    "3x4" : 12,
-    "3x5" : 15,
-    "3x6" : 18,
-    "3x7" : 21,
-    "3x8" : 24,
-    "3x9" : 27,
-    "3x10" : 30,
-    "3x11" : 33,
-    "3x12" : 36,
-
-
+function multiplication(table, ...nums){
+    for (let num of nums){
+        for (let i = 1; i <= 12; i++){
+            table[`${num}x${i}`] = num * i;
+        }
+    }
+    return table;
 }
 
 //This object could be created dynamically with a function
-const division = {
-    "144/12" : 12,
-    "132/12" : 11,
-    "120/10" : 10,
-    "108/9" : 9,
+function division(table, ...nums){
+    for (let num of nums){
+        for (let i = 1; i <= 12; i++){
+            let product = num * i;
+            table[`${product}/${num}`] = i;
+        }
+    }
+    return table;
+}
+
+function produceTable(m = true, d = false, ...nums){
+    table = {}
+    if (m){
+        table = multiplication(table, ...nums);
+    }
+    if (d){
+        table = division(table, ...nums);
+    }
+    return table;
+}
+
+
+const allMultTables = produceTable(true, false, 1,2,3,4,5,6,7,8,9,10,11,12);
+
+function getKeys(table, timesTable){
+    const valuesSet = new Set(Object.values(timesTable));
+    const alphabet = "abcdefghijklmnopqrstuvwxyz"
+    for (let letter of alphabet){
+        const values = Array.from(valuesSet);
+        let randomIndex = Math.floor( Math.random() * values.length);
+        const key = values[randomIndex];
+        const newObject = {};
+        newObject[key] = [];
+        table[letter] = newObject;
+        valuesSet.delete(key);
+    }
+    return table;
+}
+function getValues(timesTable){
+    for (let item in legend){
+        const itemKeys = Object.keys(legend[item]);
+        const value = Number(itemKeys[0]);
+        for (let answer in allMultTables){
+            if (value === allMultTables[answer]){
+                legend[item][value].push(answer);
+            }
+        }
+    }
 }
 
 
 //This function should loop through the alphabet and assign values 
 //and problems to each letter to create a legend for the message
-function makeLegend(mathType){
-    const values = Object.values(mathType);
-    const alphabet = "abcdefghijklmnopqrstuvwxyz"
-    const legend = {}
-    const usedVals = []
-    for (let letter of alphabet){
-        let randomIndex = Math.floor( Math.random() * values.length);
-        const key = values[randomIndex]
-        const newObject = {};
-        newObject[key] = [];
-        legend[letter] = newObject;
-        usedVals.push(key);
-    }
-    
-    for (let item in legend){
-        const itemKeys = Object.keys(legend[item])
-        const value = Number(itemKeys[0])
-        for (let answer in mathType){ 
-            if (value === mathType[answer] ){
-                legend[item][value].push(answer)
-            }
-        }
-    }
-    console.log(legend);
+function makeLegend(timesTable){
+    legend = getKeys({}, timesTable);
+    getValues();
     return legend;
 }
 
-makeLegend(multiplication);
+
+function encodeMessage(message, legend){
+    message = message.toLowerCase();
+    const words = message.split(" ");
+    wordsProblems = words.map(word => {
+        let encodedWord = [];
+        for (let letter of word){
+            let count = 0;
+            const problems = Object.values(legend[letter]);
+            const random = Math.floor(Math.random() * problems[0].length)
+            const problem = problems[0][random];
+            encodedWord.push(problem);
+        
+        }
+        
+        return encodedWord.join(", ");
+    })
+    
+    let encodedMessage = wordsProblems.join("   ||   ");
+    return encodedMessage;
+}
+
+let encodeForm = document.querySelector("#encodeForm");
+let mathMessage = document.querySelector("#mathMessage");
+let docLegend = document.querySelector("#legend");
+
+encodeForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    let legend = makeLegend(allMultTables);
+    console.log(legend);
+    let secretMessage = document.querySelector("#secretMessage").value;
+    let encoded = encodeMessage(secretMessage, legend);
+    document.querySelector("#secretMessage").value = '';
+    mathMessage.innerHTML = encoded;
+    for (let item in legend){
+        const value = Object.keys(legend[item]);
+        const line = document.createElement("div");
+        line.innerHTML = `${value} : ${item}`
+        docLegend.append(line);
+    }
+    
+})
+
