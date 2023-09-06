5014bfe97829c6d14b95e950b40f91798b6f0248
Megan Buck <thequiltingriverotter@gmail.com>
2023-09-05 21:10:21 -0400

    Changed division function to ensure enough values for alphabet

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //This object could be created dynamically with a function
-function multiplication(table, ...nums){
-    for (let num of nums){
-        for (let i = 1; i <= 12; i++){
+function multiplication(table, ...nums) {
+    for (let num of nums) {
+        for (let i = 1; i <= 12; i++) {
             table[`${num}x${i}`] = num * i;
         }
     }
@@ -9,9 +9,9 @@ function multiplication(table, ...nums){
 }
 
 //This object could be created dynamically with a function
-function division(table, ...nums){
-    for (let num of nums){
-        for (let i = 1; i <= 12; i++){
+function division(table, ...nums) {
+    for (let num of nums) {
+        for (let i = 1; i <= 27; i++) {
             let product = num * i;
             table[`${product}/${num}`] = i;
         }
@@ -19,26 +19,26 @@ function division(table, ...nums){
     return table;
 }
 
-function produceTable(m = true, d = false, ...nums){
+function produceTable(m = true, d = false, ...nums) {
     table = {}
-    if (m){
+    if (m) {
         table = multiplication(table, ...nums);
     }
-    if (d){
+    if (d) {
         table = division(table, ...nums);
     }
     return table;
 }
 
 
-const allMultTables = produceTable(true, false, 1,2,3,4,5,6,7,8,9,10,11,12);
+const allMultTables = produceTable(true, false, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12);
 
-function getKeys(table, timesTable){
+function getKeys(table, timesTable) {
     const valuesSet = new Set(Object.values(timesTable));
     const alphabet = "abcdefghijklmnopqrstuvwxyz"
-    for (let letter of alphabet){
+    for (let letter of alphabet) {
         const values = Array.from(valuesSet);
-        let randomIndex = Math.floor( Math.random() * values.length);
+        let randomIndex = Math.floor(Math.random() * values.length);
         const key = values[randomIndex];
         const newObject = {};
         newObject[key] = [];
@@ -47,12 +47,12 @@ function getKeys(table, timesTable){
     }
     return table;
 }
-function getValues(timesTable){
-    for (let item in legend){
+function getValues(timesTable) {
+    for (let item in legend) {
         const itemKeys = Object.keys(legend[item]);
         const value = Number(itemKeys[0]);
-        for (let answer in allMultTables){
-            if (value === allMultTables[answer]){
+        for (let answer in allMultTables) {
+            if (value === allMultTables[answer]) {
                 legend[item][value].push(answer);
             }
         }
@@ -62,30 +62,30 @@ function getValues(timesTable){
 
 //This function should loop through the alphabet and assign values 
 //and problems to each letter to create a legend for the message
-function makeLegend(timesTable){
+function makeLegend(timesTable) {
     legend = getKeys({}, timesTable);
     getValues();
     return legend;
 }
 
 
-function encodeMessage(message, legend){
+function encodeMessage(message, legend) {
     message = message.toLowerCase();
     const words = message.split(" ");
     wordsProblems = words.map(word => {
         let encodedWord = [];
-        for (let letter of word){
+        for (let letter of word) {
             let count = 0;
             const problems = Object.values(legend[letter]);
             const random = Math.floor(Math.random() * problems[0].length)
             const problem = problems[0][random];
             encodedWord.push(problem);
-        
+
         }
-        
+
         return encodedWord;
     })
-    
+
     console.log(wordsProblems)
     return wordsProblems;
 }
@@ -95,16 +95,16 @@ let mathMessage = document.querySelector("#mathMessage");
 let docLegend = document.querySelector("#legend");
 
 
-function showMathMessage(message){
-    while (mathMessage.firstChild){
+function showMathMessage(message) {
+    while (mathMessage.firstChild) {
         mathMessage.removeChild(mathMessage.firstChild)
     }
     const messageForm = document.createElement("form")
     messageForm.classList.add("messageForm")
-    for (let word of message){
+    for (let word of message) {
         const w = document.createElement("div");
         w.classList.add("word");
-        for (let problem of word){
+        for (let problem of word) {
             const cell = document.createElement("div")
             cell.classList.add('problem')
             const input = document.createElement("input");
@@ -132,7 +132,7 @@ function showMathMessage(message){
 
 encodeForm.addEventListener("submit", (e) => {
     e.preventDefault();
-    while (docLegend.firstChild){
+    while (docLegend.firstChild) {
         docLegend.removeChild(docLegend.firstChild);
     }
     let legend = makeLegend(allMultTables);
@@ -140,12 +140,12 @@ encodeForm.addEventListener("submit", (e) => {
     let encoded = encodeMessage(secretMessage, legend);
     document.querySelector("#secretMessage").value = '';
     showMathMessage(encoded);
-    for (let item in legend){
+    for (let item in legend) {
         const value = Object.keys(legend[item]);
         const line = document.createElement("div");
         line.classList.add('line');
         line.innerHTML = `${item.toUpperCase()}   -    ${value}`
         docLegend.append(line);
     }
-    
+
 })
